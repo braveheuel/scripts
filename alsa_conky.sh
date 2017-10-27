84d823d5f73c222644f3a1a52155b88fa0cbd32e
Christoph Heuel <mail@christoph-heuel.net>
2017-10-28 01:10:44 +0200

    Tune Mixer script

--- alsa_conky.sh
+++ alsa_conky.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "muted" } else { printf sprintf($2)}}'
+amixer -c 1 get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "muted" } else { printf sprintf($2)}}'
